12b484b3e88921c27b1920e19a78c2bf62010684
miha11-hash <d_mihaela@msn.com>
2024-03-28 11:15:25 +0200

    main structure & header

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from "react-native";
+import Header from "./components/header";
 
 export default function App() {
+  const[rodos, setTodos]=React.useState([
+    {text: "buy a coffee", key: 1}, 
+    {text: "create an app", key: 2},
+    {text: "play on the switch", key: 3}
+  ])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!!!!!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        {/* todos form*/}
+        <View style={styles.list}>
+          <FlatList data={todos} renderItem={({item})=> (
+            <Text>{item.text}</Text>
+          )}   />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +27,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
+  }
 });
